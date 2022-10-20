238bead8b9df8a46b17429eab3cba6a7683d55b6
Mwarstep <mvoi811@gmail.com>
2022-10-20 05:24:30 -0700

    Updates -Mwarstep

--- main.h
+++ main.h
@@ -1,41 +1,28 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
 #include <stdarg.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
 
 
 
 #define UNUSED(x) (void)(x)
-
 #define BUFF_SIZE 1024
 
 
 
 /* FLAGS */
-
 #define F_MINUS 1
-
 #define F_PLUS 2
-
 #define F_ZERO 4
-
 #define F_HASH 8
-
 #define F_SPACE 16
 
 
 
 /* SIZES */
-
 #define S_LONG 2
-
 #define S_SHORT 1
-
 
 
 /**
@@ -63,7 +50,6 @@ struct fmt
 */
 
 typedef struct fmt fmt_t;
-
 
 int _printf(const char *format, ...);
 
@@ -111,8 +97,8 @@ int print_hexa_upper(va_list types, char buffer[],
 
 
 
-int print_hexa(va_list types, char map_to[],
-	       char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags,
+		char flag_ch, int width, int precision, int size);
 
 
 
