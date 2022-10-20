a1d554d0732a527db1abb9b37e35c293a45cedb7
Mwarstep <mvoi811@gmail.com>
2022-10-20 00:00:06 -0700

    Bug fixed on Handle Print file - Mwarstep

--- handle_print.c
+++ handle_print.c
@@ -19,7 +19,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{ 'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
@@ -34,6 +34,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		if (fmt[*ind] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
